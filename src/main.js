7e8da341bc6730fe51155a2d6773accb2c983796
Andy <andy@Andys-MacBook-Pro.local>
2021-11-22 23:57:42 -0500

    new apolloClient

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core'
+
 import App from './App.vue'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
 
 createApp(App).mount('#app')
