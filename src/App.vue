ce31c8550a9ac93d3239145aa7d30f407b89e969
Andy <andy@Andys-MacBook-Pro.local>
2021-11-29 03:08:45 -0500

    style

--- src/App.vue
+++ src/App.vue
@@ -31,3 +31,24 @@ export default {
   },
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.list-wrapper {
+  display: flex;
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+.list {
+  width: 50%;
+}
+</style>
