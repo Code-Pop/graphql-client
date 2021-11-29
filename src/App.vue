cb42ca481c4675c6c9b0270c92bf479dcf061be1
Andy <andy@Andys-MacBook-Pro.local>
2021-11-29 03:10:31 -0500

    reset App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div></div>
 </template>
+
+<script>
+</script>
 
 <style>
 #app {
@@ -17,5 +13,15 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.list-wrapper {
+  display: flex;
+  margin: 0 auto;
+  max-width: 960px;
+}
+
+.list {
+  width: 50%;
 }
 </style>
