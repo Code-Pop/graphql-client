dbb48d77f8f65b404d11290cab5ee439b05a7ec7
Andy <andy@Andys-MacBook-Pro.local>
2022-01-08 18:11:38 -0500

    useResult

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <p v-if="loading">Loading...</p>
     <p v-else-if="error">Something went wrong! Please try again</p>
     <template v-else>
-      <p v-for="book in result.allBooks" :key="book.id">
+      <p v-for="book in books" :key="book.id">
         {{ book.title }}
       </p>
     </template>
@@ -13,7 +13,7 @@
 
 <script>
 import { ref } from 'vue'
-import { useQuery } from '@vue/apollo-composable'
+import { useQuery, useResult } from '@vue/apollo-composable'
 import ALL_BOOKS_QUERY from './graphql/allBooks.query.gql'
 
 export default {
@@ -21,16 +21,19 @@ export default {
   setup() {
     const searchTerm = ref('')
     const { result, loading, error } = useQuery(
-      ALL_BOOKS_QUERY, 
-      () => ({ 
-        search: searchTerm.value 
+      ALL_BOOKS_QUERY,
+      () => ({
+        search: searchTerm.value,
       }),
       () => ({
-        debounce: 500
+        debounce: 500,
+        enabled: searchTerm.value.length > 2
       })
     )
 
-    return { result, searchTerm, loading, error }
+    const books = useResult(result, [], data => data.allBooks)
+
+    return { books, searchTerm, loading, error }
   },
 }
 </script>
