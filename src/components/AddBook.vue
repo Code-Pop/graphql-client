9f1fbcf5646e7949471f9f325134d38e5594142c
Andy <andy@Andys-MacBook-Pro.local>
2022-02-09 01:21:34 -0500

    model and submit

--- src/components/AddBook.vue
+++ src/components/AddBook.vue
@@ -1,24 +1,24 @@
 <template>
-  <form>
+  <form @submit.prevent="addBook"> 
     <label for="title">
       Title
-      <input type="text" id="title" required />
+      <input type="text" id="title" required v-model.trim="newBook.title" />
     </label>
     <label for="author">
       Author
-      <input type="text" id="author" required />
+      <input type="text" id="author" required v-model.trim="newBook.author" />
     </label>
     <label for="description">
       Description (optional)
-      <input type="text" id="description" />
+      <input type="text" id="description" v-model.trim="newBook.description" />
     </label>
     <label for="year">
       Year
-      <input type="number" id="year" required />
+      <input type="number" id="year" required v-model.number="newBook.year" />
     </label>
     <label for="rating">
       Rating (optional)
-      <input type="number" id="rating" />
+      <input type="number" id="rating" v-model.number="newBook.rating" />
     </label>
     <button type="submit">Submit</button>
     <button type="reset" @click="$emit('closeForm')">Close form</button>
