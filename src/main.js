2cad18c82c12c53a9cd9b3bda2170e5db7249628
Andy <andy@Andys-MacBook-Pro.local>
2022-02-16 21:56:06 -0500

    split

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import {
   ApolloClient,
   createHttpLink,
   InMemoryCache,
+  split
 } from '@apollo/client/core'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { WebSocketLink } from '@apollo/client/link/ws'
 
 import App from './App.vue'
+import { getMainDefinition } from '@apollo/client/utilities'
 
 const wsLink = new WebSocketLink({
   uri: 'ws://localhost;4000/graphql',
@@ -19,6 +21,18 @@ const wsLink = new WebSocketLink({
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
 })
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    )
+  },
+  wsLink,
+  httpLink
+)
 
 const cache = new InMemoryCache()
 
